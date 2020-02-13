0b293833419041321562a1ef30771cbd920ea343
Dexter Dagorne <dj.dagorne@gmail.com>
2020-02-12 23:20:46 -0500

    fixes

--- src/config.jsx
+++ src/config.jsx
@@ -1,4 +1,4 @@
 export default {
-    API_ENDPOINT: 'http://localhost:8000/api' || process.env.REACT_APP_API_BASE_URL,
+    API_ENDPOINT: /* 'http://localhost:8000/api' || */ process.env.REACT_APP_API_BASE_URL,
     TOKEN_KEY: process.env.REACT_APP_API_TOKEN_KEY
 }
